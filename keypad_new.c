d4fe1548b9335e9471f4314fb32d949fb980adc2
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-04-14 16:57:30 +0200

    - spacing

--- keypad_new.c
+++ keypad_new.c
@@ -1,25 +1,25 @@
 #include "keypad_new.h"
 
 void read_ID() {
-int i=0;
-while (i<ID_SIZE)
-{
-  key = keypad.getKey();
-  if (key!= NO_KEY){
-    array_ID[i] = key;
-    i++;
+  int i=0;
+  while (i<ID_SIZE)
+  {
+    key = keypad.getKey();
+    if (key!= NO_KEY){
+      array_ID[i] = key;
+      i++;
+    }
   }
-}
 }
 
 void read_PIN() {
-int i=0;
-while (i<PIN_SIZE)
-{
-  key = keypad.getKey();
-  if (key!= NO_KEY){
-    array_PIN[i] = key;
-    i++;
+  int i=0;
+  while (i<PIN_SIZE)
+  {
+    key = keypad.getKey();
+    if (key!= NO_KEY){
+      array_PIN[i] = key;
+      i++;
+    }
   }
-}
 }
