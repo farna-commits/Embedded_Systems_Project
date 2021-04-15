eada33a4a38ad5634d79ccf08cd3a221ef09c8c9
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-04-15 13:57:09 +0200

    - removed PIN

--- keypad_new.c
+++ keypad_new.c
@@ -11,15 +11,3 @@ void read_ID() {
     }
   }
 }
-
-void read_PIN() {
-  int i=0;
-  while (i<PIN_SIZE)
-  {
-    key = keypad.getKey();
-    if (key!= NO_KEY){
-      array_PIN[i] = key;
-      i++;
-    }
-  }
-}
