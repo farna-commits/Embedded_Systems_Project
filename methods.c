688c299403e692a2b8279325dc67531468c163f6
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-04-18 01:01:09 +0200

    - removed generate key for now - Issue: DH1 alteres memory and makes error of not working when in .c file - added other encapsulating functions to clean main

--- methods.c
+++ methods.c
@@ -1,47 +1,46 @@
 #include "methods.h"
 
+#define GENERATE_KEY(X, Y)    Curve25519::dh1(X, Y)
+
 void read_ID() {
   int i=0;
   while (i<ID_SIZE)
   {
-    key = keypad.getKey();
-    if (key!= NO_KEY){
-      array_ID[i] = key;
+    key2 = keypad.getKey();
+    if (key2!= NO_KEY){
+      array_ID[i] = key2;
       i++;
     }
   }
 }
 
-
-uint8_t * generate_key(uint8_t * f,uint8_t * k) {
-    // Generate the secret value "f" and the public value "k".
-    Curve25519::dh1(k, f);
-
-
-    // Generate the shared secret in "k" using the previous secret value "f".
-    if (!Curve25519::dh2(k, f)) {
-        // The received "k" value was invalid - abort the session.
-    Serial.println("encryption failed");
-    }
-    // The "k" value can now be used to generate session keys for encryption.
-    Serial.println("Key generated successfully");
-
-    return k;
-    
+DeserializationError Read_json(StaticJsonDocument<700>, char*) {
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, json);
+  // Test if parsing succeeds.
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }  
+  return error; 
 }
 
-void AES_encrypt(char * data) {
-    const uint8_t KEY_SIZE = 16; 
-    uint8_t f[KEY_SIZE];
-    uint8_t k[KEY_SIZE];
+char * align_ID_string(int ID_example, char a[16]) {
+  itoa(ID_example, a, 10);
+  strcat(a,"00000000000");
+  return a; 
+}
 
-    uint8_t key[KEY_SIZE];
-    //key = generate_key(f,k);
-    aes128_enc_single(key, data);
-    Serial.print("encrypted:");
-    Serial.print(data[0]);      
-    Serial.print(data[1]);
-    Serial.print(data[2]);
-    Serial.println(data[3]);
+void AES_encrypt(uint8_t * key, char * buf ) {
+  aes128_enc_single(key, buf);
+  Serial.print("Encrypted ID:");
+  Serial.println(buf);
+  aes128_dec_single(key, buf);
+  Serial.print("Decrypted ID:");
+  Serial.print(buf[0]);
+  Serial.print(buf[1]);
+  Serial.print(buf[2]);
+  Serial.print(buf[3]);
 
 }
