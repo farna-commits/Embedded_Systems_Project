a9daa2424363145952b092ee471634e3e46c432b
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-04-17 22:43:34 +0200

    - main working without methods - tested without keypad

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,4 @@
-#include "D:\AUC\Semester10(Spring2021)\Embedded\Project\repo\Embedded_Systems_Project\keypad_new.c"
-#include "D:\AUC\Semester10(Spring2021)\Embedded\Project\repo\Embedded_Systems_Project\database_json.h"
-#include "D:\AUC\Semester10(Spring2021)\Embedded\Project\repo\Embedded_Systems_Project\diffie_hellman.h"
-#include "D:\AUC\Semester10(Spring2021)\Embedded\Project\repo\Embedded_Systems_Project\AES128.c"
+#include "D:\AUC\Semester10(Spring2021)\Embedded\Project\repo\Embedded_Systems_Project\methods.h"
 #include <string.h>
 
 
@@ -20,16 +17,32 @@ while (!Serial) continue;
     return;
   }  
 
-  int temp = doc["ID"][49];
+  int temp = doc["ID"][1];
   
   Serial.println(temp);
-  char buf[4];
+  char buf[16];
   // Convert number to string [buf]
   itoa(temp, buf, 10);
   Serial.println(buf);
-  strcat(buf,"0000000000000000");
+  strcat(buf,"00000000000");
   Serial.println(buf);
-  AES_encrypt("123");
+  // uint8_t a[32], b[32], c[32]; 
+  uint8_t key[16];
+  uint8_t b[16];
+  uint8_t *c; 
+  //c = generate_key(a,b);
+  Curve25519::dh1(key, b);
+  Serial.println(key[0]);
+  aes128_enc_single(key, buf);
+  Serial.print("encrypted:");
+  Serial.println(buf);
+  aes128_dec_single(key, buf);
+  Serial.print("decrypted:");
+  Serial.print(buf[0]);
+  Serial.print(buf[1]);
+  Serial.print(buf[2]);
+  Serial.print(buf[3]);
+  // AES_encrypt("123");
 }
 
 void loop() {
