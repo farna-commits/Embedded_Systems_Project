84fd11bef86452b06cde3a637c9a934db243735f
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-04-21 22:56:21 +0200

    - added enum for packet header constants as chars - added packet header argument

--- methods.h
+++ methods.h
@@ -34,7 +34,10 @@ StaticJsonDocument<700> doc;
 char json[] = "{\"ID\": [2606, 3405, 2303, 2073, 1635, 3668, 1133, 1968, 1440, 2806, 3773, 2220, 3912, 2251, 3870, 2362, 1236, 3161, 1593, 3211, 2797, 1388, 1272, 3016, 2290, 1768, 2186, 1928, 2013, 2561, 1689, 3988, 1182, 2061, 1848, 2305, 2053, 3479, 2506, 2032, 3678, 2472, 3909, 3137, 3645, 2761, 2247, 2899, 2270, 1900]}";
 uint16_t array_ID[ID_SIZE]    = {};
 uint8_t proto_buffer[MAX_BUFFER_SIZE]; 
-
+enum Packet_Header: char {
+  ID_HEADER         = 'h',
+  DIFFIE_PUBLIC_KEY = 'p'
+};
 
 //Functions 
 void AES_encrypt(uint8_t * key, uint8_t * buf );
@@ -42,7 +45,7 @@ void AES_decrypt(uint8_t * key, char * buf);
 DeserializationError Read_json(StaticJsonDocument<700>, char*); 
 void align_ID_string(int, char a[]);
 void onFrameIn(uint8_t *buf, int len); 
-void send_packet(uint16_t, char *); 
+void send_packet(uint16_t, char *, Packet_Header); 
 
 
 #ifdef __cplusplus
