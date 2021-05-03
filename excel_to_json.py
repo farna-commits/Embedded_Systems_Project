bd1df89be050c6738810aeb279949567fb5fd862
Osama El Farnawany <57485975+farna-commits@users.noreply.github.com>
2021-05-03 16:58:59 +0200

    - 25 db size

--- excel_to_json.py
+++ excel_to_json.py
@@ -4,14 +4,15 @@ import hashlib
 def transofrm():
     df = pd.read_excel (r'database.xls')
     list = []
-    for i in range (0,len(df)):
+    DATABASE_SIZE = 25 
+    for i in range (0, DATABASE_SIZE):
         list.append(int(df.values[i]))
     list_strings = [str(int) for int in list]
     return list_strings 
 
 def hash_sha256(list): 
     hashed_list = []
-    for i in range(0,25):
+    for i in range(0,len(list)):
         l = hashlib.sha256(list[i].encode())
         hashed_list.append(l.hexdigest())
     return hashed_list
@@ -19,7 +20,6 @@ def hash_sha256(list):
 if __name__ == "__main__":
     list = transofrm()
     hashed_list = hash_sha256(list)
-    print(len(hashed_list))
     hashed_list2 = []
     print(f"Original list: \n {list} \n")
     print(f"Hashed list: \n {hashed_list} \n")
